b49cec4f8182238e66c66432a626ec284f1f74bd
Ali Raza <alisabri022@gmail.com>
2021-06-17 13:42:51 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import FormFormik from "./Components/Forms";
+import SignIn from "./Components/SignIn";
+import { auth } from "./Components/firebase";
+const handleAuth = async () => {
+  try {
+    const user = await auth.currentUser;
+    if (user !== null) {
+      console.log(user.email);
+    } else {
+      console.log("no user present");
+    }
+  } catch (error) {
+    if (error) console.log(error);
+  }
+};
+const handleSignOut = async () => {
+  try {
+    const user = await auth.signOut();
+  } catch (error) {
+    if (error) console.log(error);
+  }
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FormFormik />
+      <SignIn />
+      <button className="btn btn-primary mt-5" onClick={handleAuth}>
+        check user
+      </button>
+      <button className="btn btn-primary mt-5 ml-5" onClick={handleSignOut}>
+        Sign Out
+      </button>
     </div>
   );
 }
